4f756565dc1155ff0a15e84259b858d0921a75e0
liamjeon <89363456+liamjeon@users.noreply.github.com>
2022-01-09 00:19:38 +0900

    다형성 - polymorphism

---
+++ 3-oop/3-6-polymorphism.ts
@@ -0,0 +1,112 @@
+{
+  //public
+  //private
+  //protected
+
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT: number = 7;
+    private coffeeBeans: number = 0;
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error("value for beans should be greater than 0");
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean(): void {
+      console.log("cleaning the machine...");
+    }
+
+    private grindBeans(shots: number): void {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("Not enough coffee beans!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log("heating up...");
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`Pulling ${shots}`);
+      return {
+        shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(shots: number, public readonly serialNumber: string) {
+      super(shots);
+    }
+
+    private steamMilk(): void {
+      console.log("Steaming some milk...");
+    }
+    //overwrting
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  //다형성의 장점 :
+  // 내부적으로 구현된 다양한 클래스들이 한 가지의 인터페이스를 구현하거나
+  //또는 동일한 부모 클래스를 상속했을 때 어떤 클레스인지 구분하지 않고 공통된 api를 호출할 수 있다.
+
+  const machines: CoffeeMaker[] = [
+    new CoffeeMachine(32),
+    new CaffeLatteMachine(32, "S123"),
+    new SweetCoffeeMaker(32),
+    new CoffeeMachine(32),
+    new CaffeLatteMachine(32, "S123"),
+    new SweetCoffeeMaker(32),
+  ];
+
+  machines.forEach((machine) => {
+    console.log("-------------------------");
+    machine.makeCoffee(1);
+  });
+}
