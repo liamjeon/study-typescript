d790049bd1428efdf2fa33d1dbc4bd6e9135d6b3
liamjeon <89363456+liamjeon@users.noreply.github.com>
2022-01-11 16:24:11 +0900

    [update] OOP - stack project 완료

--- 4-oop-project/stack.ts
+++ 4-oop-project/stack.ts
@@ -1,20 +1,47 @@
 {
   interface Stack {
-    size: number; //readonly : 안에서만 변경가능
-    push(str: string): void;
+    readonly size: number;
+    push(value: string): void;
     pop(): string;
   }
 
-  class List implements Stack {
-    constructor(public size: number, private arr: string[]) {
-      this.size = 0;
+  type StackNode = {
+    readonly value: string;
+    readonly next?: StackNode; //next? : 값이 있거나 없거나, optional ; StackNode | undefined
+  };
+
+  class StackImpl implements Stack {
+    private _size: number = 0; //내부에서만 쓰이는 변수는 _ 언더바 붙힘
+    private head?: StackNode;
+
+    get size() {
+      return this._size;
     }
-    public push(str: string): void {
-      this.arr[this.size++] = str;
+    push(value: string) {
+      const node: StackNode = {
+        value,
+        next: this.head,
+      };
+      this.head = node;
+      this._size++;
     }
-    public pop(): string {
-      return this.arr[this.size--];
+    pop(): string {
+      //null == undefined, null !== undefined
+      if (this.head == null) {
+        throw new Error("Stack is empty");
+      }
+      const node = this.head;
+      this.head = node.next;
+      this._size--;
+      return node.value;
     }
   }
+
+  const stack = new StackImpl();
+  stack.push('liam');
+  stack.push('julie');
+
+  while(stack.size !== 0){
+    console.log(stack.pop());
+  }
 }
-    
